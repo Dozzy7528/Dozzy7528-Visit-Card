2f3675323443129c9a5bc11a9d4686e37a547cb8
Dozzy <106274005+Dozzy7528@users.noreply.github.com>
2024-08-25 20:11:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener('mousemove', function(e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;
     let body = document.querySelector('body');
-    body.style.background = `radial-gradient(circle at ${x * 100}% ${y * 100}%, #ff0066, #0d0d0d)`;
+    body.style.background = `radial-gradient(circle at ${x * 100}% ${y * 100}%, #333, #0d0d0d)`;
 });
