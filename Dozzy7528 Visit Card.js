628614322bfa79aa3fbcb4c69504996e1f9d88a4
Dozzy <106274005+Dozzy7528@users.noreply.github.com>
2024-08-25 19:42:02 +0300

    Delete Dozzy7528 Visit Card.js

--- Dozzy7528 Visit Card.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const x = e.clientX / window.innerWidth;
-    const y = e.clientY / window.innerHeight;
-    const bgColor = `rgba(${Math.floor(x * 255)}, ${Math.floor(y * 255)}, 255, 0.1)`;
-    document.body.style.background = bgColor;
-});
