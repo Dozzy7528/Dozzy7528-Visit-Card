6c52cd4e8d666cbe17a2050916a68e70bbcbf3dd
Dozzy <106274005+Dozzy7528@users.noreply.github.com>
2024-08-25 21:48:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.addEventListener('mousemove', function(e) {
     const body = document.body;
 
     // Вычисляем цвета на основе положения мыши
-    const red = Math.floor(x * 2 * 255);
-    const green = Math.floor(y * 2 * 255);
-    const blue = Math.floor((x + y) / 3 * 255);
+    const red = Math.floor(x * 255);
+    const green = Math.floor(y * 255);
+    const blue = Math.floor((x + y) / 2 * 255);
 
     // Устанавливаем радиальный градиент как фон
     body.style.background = `radial-gradient(circle at ${x * 100}% ${y * 100}%, rgb(${red}, ${green}, ${blue}), rgb(${255 - red}, ${255 - green}, ${255 - blue}))`;
